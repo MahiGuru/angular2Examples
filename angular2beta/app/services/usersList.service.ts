c46b6fdd32dcce01ac0d8f254dbd6467088ad24f
Mahipal <mahi65351@gmail.com>
2016-05-18 23:52:07 +0530

    Removed angular beta folder

--- angular2beta/app/services/usersList.service.ts
+++
@@ -1,9 +0,0 @@
-export class UserList{
-    serviceName : string = "UserList Service";
-    ListofUsers : string[] = ["Mahipal", "Lucky", "Savi", "akki"]    
-    
-    getUsers():string[]{
-     return ["Mahipal", "Lucky", "Savi", "akki"];
-    }
-    
-}
